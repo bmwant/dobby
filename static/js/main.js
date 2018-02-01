88d1f4fb3e9b0ff0444eb7396d8c7554634e1ca3
Misha Behersky <bmwant@gmail.com>
2018-02-01 14:33:45 +0200

    Append all pictures to playing field

--- static/js/main.js
+++ static/js/main.js
@@ -1,13 +1,25 @@
 $(function() {
-    if (SVG.supported) {
-      var draw = SVG('drawing').size(300, 300);
-      var rect = draw.rect(100, 100).attr({ fill: '#f06' });
-      console.log('Should be good');
-    } else {
-      alert('SVG not supported');
+    if (!SVG.supported) {
+      alert('SVG is not supported');
       return;
     }
 
-    var draw = SVG('image').size(640, 640);
-    var image = draw.image('/static/img/001_eye.svg');
+    var images = [
+        '/static/img/001_eye.svg',
+        '/static/img/002_bottle.svg',
+        '/static/img/003_pin.svg',
+        '/static/img/004_bear.svg',
+        '/static/img/005_gift.svg',
+        '/static/img/006_cubes.svg',
+        '/static/img/007_car.svg',
+        '/static/img/008_snow.svg'
+    ];
+
+    $.each(images, function(index, value) {
+        var imagePath = images[index];
+        var imageBlockId = 'image' + index;
+        $('<div/>').attr('id', imageBlockId).appendTo('#field');
+        var draw = SVG(imageBlockId).size(128, 128);
+        draw.image(imagePath);
+    });
 });
